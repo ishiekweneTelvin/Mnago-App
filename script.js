84582e201974482eb6ea7f957f2ae33e06842a77
TELVIN ISHIEKWENE <T.I.ISHIEKWENE-2021@hull.ac.uk>
2022-02-15 17:17:49 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let payments = document.getElementById('payments');
+
+let paymentsdropdwon = document.getElementById('paymentsdropdwon');
+
+
+payments.addEventListener(onclick, show)
+
+
+function show(){
+    paymentsdropdwon.style.display = 'none'
+}
